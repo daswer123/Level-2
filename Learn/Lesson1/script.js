2e2e79d0797d1c329c24e796816e24c389f5a532
daswer123 <daswerq123@gmail.com>
2020-08-14 21:47:28 +0300

    First practis and lessons

---
+++ Learn/Lesson1/script.js
@@ -0,0 +1,18 @@
+
+
+let hello = 2;
+let hi = "Hello";
+let you = true;
+let nol = null;
+let undf;
+
+
+console.log(typeof(hello), typeof(hi), typeof(you), typeof(nol), typeof(undf));
+
+let a = 1;
+a = prompt("You are normalies", "No");
+console.log(a);
+
+let b = "34";
+b = confirm ("Isn't it?");
+alert(b);
