189546a91667d393c7b0ec9c0f81caa8a61cca1c
daswer123 <daswerq123@gmail.com>
2020-08-19 12:57:35 +0300

    pracice 8 - tabs

---
+++ Learn/Lessons/Homework/Practice 8 - Tabs/js/script.js
@@ -0,0 +1,36 @@
+
+const tabs = document.querySelectorAll(".tabcontent"),
+      tabToggle = document.querySelector(".tabheader__items");
+
+
+tabToggle.addEventListener("click",(event) => {
+
+    if(event.target && event.target.classList.contains("tabheader__item")){
+
+        let tab = event.target;
+        let tabIndex = 0;
+        tabToggle.querySelectorAll(".tabheader__item").forEach((elem,i) =>{
+        if(elem.classList.contains("tabheader__item_active")){
+             elem.classList.remove("tabheader__item_active");
+        }
+
+        if(elem == tab){
+            tabIndex = i;
+        }
+
+        });
+
+        tab.classList.add("tabheader__item_active");
+
+        
+        tabs.forEach((elem,i) => {
+            if (elem.classList.contains("tabcontent-active")){
+                elem.classList.remove("tabcontent-active");
+            }
+            if( i == tabIndex){
+                elem.classList.add("tabcontent-active");
+            }
+        });
+        
+    }
+});
